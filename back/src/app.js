0afcbc22da92e9ed62e06d130bd6228e5e434343
siha <supersiha@gmail.com>
2022-08-31 15:39:38 +0900

    [add] img_resizing

--- back/src/app.js
+++ back/src/app.js
@@ -8,7 +8,14 @@ import { awardRouter } from "./routers/awardRouter";
 import { certificateRouter } from "./routers/certificateRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { login_required } from "./middlewares/login_required";
+import path from "path";
 
+const sharp = require("sharp");
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer({
+  dest: `${__dirname}/../public/images/`,
+});
 const app = express();
 
 // CORS 에러 방지
@@ -32,32 +39,71 @@ app.use(projectRouter);
 app.use(awardRouter);
 app.use(certificateRouter);
 
-app.use(fileUpload());
-app.post("/upload", login_required, (req, res, next) => {
-  let uploadFile = req.files.file;
-  let fileName = req.files.file.name;
-  const fileNameArr = fileName.split(".");
-  if (
-    fileNameArr.length == 1 ||
-    !(
-      fileNameArr[1] === "jpg" ||
-      fileNameArr[1] === "jpeg" ||
-      fileNameArr[1] === "png"
-    )
-  ) {
-    throw new Error("파일 확장자 확인 : jpg, jpeg, png");
-  }
-  fileName = req.userId + "." + fileNameArr[1];
-  uploadFile.mv(__dirname + "/../public/images/" + fileName, function (err) {
-    if (err) {
-      return res.status(500).send(err);
-    }
+app.post(
+  "/upload",
+  login_required,
+  upload.single("file"),
+  async (req, res, next) => {
+    // res.status(200).send("Uploaded: " + req.file);
+    // console.log(req.file);
+    try {
+      let fileName = new Date().valueOf();
+      await sharp(req.file.path)
+        .resize({ width: 200, height: 200 })
+        .withMetadata()
+        .toFile(`${__dirname}/../public/images/${fileName}.jpg`),
+        (err, info) => {
+          if (err) throw err;
+        };
 
-    res.status(201).send({
-      imgUrl: `http://kdt-ai5-team13.elicecoding.com:5001/images/${fileName}`,
-    });
-  });
-});
+      res.status(201).send({
+        imgUrl: `http://localhost:5001/images/${fileName}.jpg`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// app.use(fileUpload());
+// app.post("/upload", login_required, (req, res, next) => {
+//   let uploadFile = req.files.file;
+//   let fileName = req.files.file.name;
+//   const fileNameArr = fileName.split(".");
+//   if (
+//     fileNameArr.length == 1 ||
+//     !(
+//       fileNameArr[1] === "jpg" ||
+//       fileNameArr[1] === "jpeg" ||
+//       fileNameArr[1] === "png"
+//     )
+//   ) {
+//     throw new Error("파일 확장자 확인 : jpg, jpeg, png");
+//   }
+//   fileName = req.userId + "." + fileNameArr[1];
+//   const filePath = `${__dirname}/../public/images/`;
+//   console.log(filePath);
+
+//   uploadFile.mv(filePath + fileName, async (err) => {
+//     if (err) {
+//       return res.status(500).send(err);
+//     }
+//     try {
+//       await sharp(filePath)
+//         .resize({ width: 200, height: 200 })
+//         .withMetadata()
+//         .toFile(filePath + fileName);
+//       console.log(uploadFile);
+
+//       res.status(201).send({
+//         imgUrl: `http://kdt-ai5-team13.elicecoding.com:5001/images/${fileName}`,
+//       });
+//     } catch (error) {
+//       next(error);
+//     }
+//   });
+
+// });
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
 
